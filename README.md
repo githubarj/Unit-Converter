f47dedd2230a238a82da043be6be8388fd217c93
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2022-07-22 18:21:22 +0300

    added themes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Unit-Converter
-This is a system that coverts metric to imperial units.
+This is a system that coverts metric to imperial units and rounds them off by two.  
+It also has a dark theme and light theme.
--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const convertBtn = document.getElementById("convert-btn");
-const numInput = document.getElementById("num-input")
+const numInput = document.getElementById("num-input");
 const length = document.getElementById("length");
 const volume = document.getElementById("volume");
 const mass = document.getElementById("mass");
+const icon = document.getElementById("icon");
 
+//dark them and light theme
+icon.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "sun.png";
+  } else {
+    icon.src = "moon.png";
+  }
+});
 
-convertBtn.addEventListener("click", function (){
-    const number = numInput.value 
-    const fAns = (number * 3.281).toFixed(2)
-    const mAns = (number / 3.281).toFixed(2)
-    length.textContent = `${number} meters = ${fAns} feet | ${number} feet = ${mAns} meters`
+convertBtn.addEventListener("click", function () {
+  const number = numInput.value;
+  const fAns = (number * 3.281).toFixed(2);
+  const mAns = (number / 3.281).toFixed(2);
+  length.textContent = `${number} meters = ${fAns} feet | ${number} feet = ${mAns} meters`;
 
+  const gAns = (number / 3.785).toFixed(2);
+  const lAns = (number * 3.785).toFixed(2);
+  volume.textContent = `${number} liters = ${gAns} gallons | ${number} gallons = ${lAns} liters`;
 
-    const gAns = (number / 3.785).toFixed(2);
-    const lAns = (number * 3.785).toFixed(2);
-    volume.textContent = `${number} liters = ${gAns} gallons | ${number} gallons = ${lAns} liters`;
-
-    
-    const pAns = (number * 2.204).toFixed(2);
-    const kAns = (number / 2.204).toFixed(2);
-    mass.textContent = `${number} kilos = ${pAns} pounds | ${number} pounds = ${kAns} kilos`;
-
-
-
-})
+  const pAns = (number * 2.204).toFixed(2);
+  const kAns = (number / 2.204).toFixed(2);
+  mass.textContent = `${number} kilos = ${pAns} pounds | ${number} pounds = ${kAns} kilos`;
+});
